61cd91d53cd9787abf0fec548757a4d9616ad2c8
esheby <esheby@gmail.com>
2018-07-03 08:55:27 +0700

    Versi dengan tabel namun memunculkan harga di messagebox

--- monte.py
+++ monte.py
@@ -3,6 +3,8 @@
 import sys
 
 import pandas as pd
+import numpy as np
+import random
 from MonteCarlo import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
@@ -14,6 +16,7 @@ class MonteCarlo(QDialog):
         self.ui.setupUi(self)
         self.ui.browseButton.clicked.connect(self.browseButtonClicked)
         self.ui.importButton.clicked.connect(self.importButtonClicked)
+        self.ui.prosesButton.clicked.connect(self.prosesButtonClicked)
 
     def browseButtonClicked(self):
         import os
@@ -46,7 +49,62 @@ class MonteCarlo(QDialog):
         self.ui.comboBahan.setEnabled(True)
         self.ui.comboHari.setEnabled(True)
         self.ui.prosesButton.setEnabled(True)
-    
+
+    def prosesButtonClicked(self):
+        pasarPilihan = self.ui.comboPasar.currentText()
+        bahanPilihan = self.ui.comboBahan.currentText()
+        self.df = pd.read_csv(str(fileHandle), index_col=[1])
+        self.df2 = self.df.loc[[pasarPilihan],[bahanPilihan]].reset_index().copy()
+        self.total = self.df2[bahanPilihan].sum()
+        print(self.total)
+        self.df2['Prob'] = self.df2[bahanPilihan] / self.total
+        self.df2['Prob Kumulatif'] = self.df2['Prob'].cumsum()
+        print(self.df2)
+        self.df2['Interval Akhir'] = self.df2['Prob Kumulatif']
+
+        self.df3 = self.df2['Interval Akhir']
+        self.interval = self.df3.iloc[:].values
+        self.interval = (self.interval * 100).round()
+
+        self.angkaRandom = []
+        for self.i in range(int(self.ui.comboHari.currentText())):
+            self.angkaRandom.append(random.randrange(0,self.df3.count()))
+        self.angkaRandom
+
+        self.c = 0
+        self.listPrediksi = []
+        self.listPenanda = []
+        for self.y in range(len(self.angkaRandom)):
+            for self.x in range(self.df3.count()):
+                if self.x == 0:
+                    self.iv = (pd.Interval(left=0, right=(self.interval[self.x]), closed ='both'))
+                    self.x+=1
+                    if (self.angkaRandom[self.y] in self.iv) == True:
+                        self.listPenanda.append(self.x)
+                        break
+                    continue
+                elif (self.interval[self.x]-self.interval[self.x-1]) == 1:
+                    self.iv = (pd.Interval(left=(self.interval[self.x]), right=(self.interval[self.x]), closed ='both'))
+                    self.x+=1
+                    if (self.angkaRandom[self.y] in self.iv) == True:
+                        self.listPenanda.append(self.x)
+                        break
+                    continue
+                else:
+                    self.iv = (pd.Interval(left=(self.interval[self.x-1]), right=(self.interval[self.x]), closed ='both'))
+                    self.x+=1
+                    if (self.angkaRandom[self.y] in self.iv) == True:
+                        self.listPenanda.append(self.x)
+                        break
+        
+        self.df4 = self.df.loc[[pasarPilihan],[bahanPilihan]].reset_index().copy()
+        harga_beras = self.df4.iloc[:,1].values
+
+        for self.penanda in self.listPenanda:
+            self.listPrediksi.append(harga_beras[self.penanda])
+            
+        QMessageBox.information(self, 'Harga', repr(self.listPrediksi))
+
 if __name__ == "__main__":
     a = QApplication(sys.argv)
     form = MonteCarlo()
