4aefbe4a978bb261fa18b921c07b9bfbcf33e2e3
esheby <esheby@gmail.com>
2018-07-16 12:27:16 +0700

    Bisa nampilin message box

--- monte2.py
+++ monte2.py
@@ -5,15 +5,20 @@ from MonteCarlo import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
-class Menu(QDialog):
-     def __init__(self, parent=None):
+class MenuForm(QDialog):
+    def __init__(self, parent = None):
         QDialog.__init__(self, parent)
         self.ui = Ui_Menu()
         self.ui.setupUi(self)
-        #self.ui.button1Pasar.clicked.connect(self.button1PasarClicked)
+        self.ui.button1Pasar.clicked.connect(self.button1PasarClicked)
+
+    def button1PasarClicked(self):
+        QMessageBox.information(self, 'tes','nyoba')
 
 if __name__ == "__main__":
     a = QApplication(sys.argv)
-    form = Menu()
+
+    form = MenuForm()
     form.show()
+
     a.exec_()
